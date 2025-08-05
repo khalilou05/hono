9c8df25782e8b57547f3efac3aedb7153e022e21
khalil <khalilbenmeziane@gmail.com>
2025-08-05 15:23:59 +0100

    added a comment

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// khalil
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
